6f1781a9d9e2440125d181e1b09639f447513217
Gianluca Nativo <giannativo@yahoo.com.ar>
2017-10-27 22:03:39 -0300

    Alfa LecturaArchivo
    Les comento lo que hice:
    En teoria lee el archivo y guarda todos los datos que se necesita y valida si coincide la cantidad de procesos que se pusieron en el numprocess con los que se cargaron. Una vez que definamos bien lo que va a contener la clase proceso modifico el add. No se que hacer con los otros atributos, si crear una clase o no, despues me dicen

--- Planificador/src/modelo/LecturaArchivo.java
+++ Planificador/src/modelo/LecturaArchivo.java
@@ -9,7 +9,18 @@ public class LecturaArchivo {
 		BufferedReader br= new BufferedReader(fr);
 		String linea;
 		int numprocess=0;
+		int meandev=0;
+		int contp=0;
+		int runtime=0;
 		boolean cargado=false;
+		int procesosintroducidos=0;
+		while((linea=br.readLine())!=null){
+			if(linea.contains("numprocess")){
+				procesosintroducidos++;
+			}
+		}
+		
+		int[] timeproceso=new int[procesosintroducidos];
 		while((linea=br.readLine())!=null){
 			StringTokenizer st= new StringTokenizer(linea);
 			if(linea.contains("numprocess")){
@@ -20,10 +31,45 @@ public class LecturaArchivo {
 		        	   cargado=true;
 		           }
 		        }
-				
+			}
+			if(numprocess!=procesosintroducidos) throw new Exception("Se cargaro una cantidad diferente de procesos a la cantidad indicada.");
+			cargado=false;
+			if(linea.contains("meandev")){
+				while (st.hasMoreTokens() && !cargado){
+		           String s2 = st.nextToken();
+		           if(esCadenaNumeros(s2)){
+		        	   meandev=Integer.valueOf(s2);
+		        	   cargado=true;
+		           }
+		        }
+			}
+			cargado=false;
+			if(linea.contains("process") && numprocess!=0){
+				while (st.hasMoreTokens() && !cargado){
+		           String s2 = st.nextToken();
+		           if(esCadenaNumeros(s2)){
+		        	   timeproceso[contp]=Integer.valueOf(s2);
+		        	   contp++;
+		        	   cargado=true;
+		           }
+		        }
+			}
+			cargado=false;
+			if(linea.contains("runtime") && numprocess!=0){
+				while (st.hasMoreTokens() && !cargado){
+		           String s2 = st.nextToken();
+		           if(esCadenaNumeros(s2)){
+		        	   runtime=Integer.valueOf(s2);
+		        	   contp++;
+		        	   cargado=true;
+		           }
+		        }
 			}
 		}
-		
+		List<Proceso> procesos=new ArrayList<Proceso>();
+		for(int i=0;i<contp;i++){
+			//procesos.add(new Proceso());
+		}
 	}
 	
 	public static boolean esCadenaNumeros(String cadena) {
