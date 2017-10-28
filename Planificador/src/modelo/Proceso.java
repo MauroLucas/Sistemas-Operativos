162bb847ed89049e476d57a968fa7a6676c910f6
aleortega <alejothomasortega@gmail.com>
2017-10-28 13:41:46 -0300

    Modifico clase Proceso
    Modifico clase Proceso para similar el programa al simulador MOSS.
    
    Elimino txt de prueba 123.
    
    Agrego un .gitigno. (ignora cierto tipos de archivos que se generan automaticamente por el IDE al iniciar el programa)

--- Planificador/src/modelo/Proceso.java
+++ Planificador/src/modelo/Proceso.java
@@ -2,46 +2,35 @@ package modelo;
 
 public class Proceso {
 	
-	private int cputime;
-	private int cantBloqueo;
-	private int cantCPU2;
+	private int cpuTime;
+	private int ioBlocking;
 	private boolean ejecutando;
 	private boolean bloqueado;
 	private boolean terminado;
 	
-	public Proceso(int cputime, int cantBloqueo, int cantCPU2, boolean ejecutando, boolean bloqueado,
-			boolean terminado) {
+	public Proceso(int cpuTime, int ioBlocking, boolean ejecutando, boolean bloqueado, boolean terminado) {
 		super();
-		this.cputime = cputime;
-		this.cantBloqueo = cantBloqueo;
-		this.cantCPU2 = cantCPU2;
+		this.cpuTime = cpuTime;
+		this.ioBlocking = ioBlocking;
 		this.ejecutando = ejecutando;
 		this.bloqueado = bloqueado;
 		this.terminado = terminado;
 	}
 
 	public int getCputime() {
-		return cputime;
+		return cpuTime;
 	}
 
 	public void setCputime(int cputime) {
-		this.cputime = cputime;
+		this.cpuTime = cputime;
 	}
 
 	public int getCantBloqueo() {
-		return cantBloqueo;
+		return ioBlocking;
 	}
 
-	public void setCantBloqueo(int cantBloqueo) {
-		this.cantBloqueo = cantBloqueo;
-	}
-
-	public int getCantCPU2() {
-		return cantCPU2;
-	}
-
-	public void setCantCPU2(int cantCPU2) {
-		this.cantCPU2 = cantCPU2;
+	public void setCantBloqueo(int ioBlocking) {
+		this.ioBlocking = ioBlocking;
 	}
 
 	public boolean isEjecutando() {
